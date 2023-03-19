dbe0a3d2273f7711e0fdbecba9641e02305fd687
Metin DEMİR <metindemir1215.md@gmail.com>
2023-03-19 20:31:17 +0300

    install tailwind

--- accessory-trade-frontend/src/App.tsx
+++ accessory-trade-frontend/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Home from "./pages/Home";
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+          </Routes>
+      </BrowserRouter>
   );
-}
-
-  function Home() {
-
-    return (
-        <div>
-            <p>xsxsxs</p>
-
-
-        </div>
-    );
 }
 
 export default App;
--- accessory-trade-frontend/src/pages/Home.tsx
+++ accessory-trade-frontend/src/pages/Home.tsx
@@ -1,13 +1,41 @@
 
+import React from "react"
+import { useState, useEffect } from "react";
 
+
+
+export  type Plate={
+    id:number,
+    name:string,
+    price:number;
+
+}
 
 export default function Home() {
+    const [plates, setPlates] = useState<Plate[]>();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
+    useEffect(() => {
+        fetch(`http://localhost:8080/accessories/all`)
+            .then((res) => res.json())
+            .then((data) => setPlates(data))
+            .catch((error) => console.log("An error occurred in Home page", error));
+    }, []);
     return (
         <div>
-         <p>xsxsxs</p>
 
 
+            <div className="flex justify-center">
+
+            </div>
+
+            <h1 className="mb-4">Home Page</h1>
+
+            <div className="grid grid-cols-3" >
+
+                  <p>vv</p>
+            </div>
         </div>
     );
 }
